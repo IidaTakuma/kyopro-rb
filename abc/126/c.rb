c8bdc7818560272437ae7399d4a5a2b272ca4b84
IidaTakuma <ta9maxxx@gmail.com>
2021-04-11 18:59:29 +0900

    abc126c

---
+++ abc/126/c.rb
@@ -0,0 +1,36 @@
+def main
+  n, k = Input.split_by_delimitor
+
+  total_prob = 0r
+  (1..n).each do |i|
+    prob = 1r
+    while i < k
+      prob /= 2r
+      i *= 2
+    end
+    total_prob += prob
+  end
+  puts (total_prob / n).to_f
+  exit
+end
+
+class Input
+  def self.one_line
+    gets
+  end
+
+  def self.one_number
+    gets.to_i
+  end
+
+  def self.split_by_delimitor(delimitor: ' ', type: 'Int')
+    case type
+    when 'Int'
+      gets.split(delimitor).map(&:to_i)
+    when 'String'
+      gets.split(delimitor)
+    end
+  end
+end
+
+main if __FILE__ == $0
